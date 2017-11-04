e8c57e88199a37fbd86401ab878ad845bb45544c
Ryan Kim <kiyonkim79@gmail.com>
2017-11-04 14:49:58 +0900

    Update server.R

--- server.R
+++ server.R
@@ -1,1 +1,13 @@
-
+library(shiny)
+shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+    dist <- switch(input$dist,
+                   Normal = rnorm,
+                   Exponential = rexp,
+                   rnorm)
+    
+    hist(dist(input$obs), col = 'yellow',
+         main = paste("Histogram of a" , input$dist, "Distribution with ", input$obs, " Observations"))  ##dist size, parameter from slider
+  })
+}
+)
