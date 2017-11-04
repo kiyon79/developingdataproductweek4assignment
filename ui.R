e24ed082fd03a098e985a3a647c4245f7f073cf5
Ryan Kim <kiyonkim79@gmail.com>
2017-11-04 14:50:23 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,25 @@
+library(shiny)
 
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("How normal and exponential distribution change as the number changes?"),
+  # Sidebar with a slider input for type of distributionT and slider input.
+  
+  sidebarLayout(
+    sidebarPanel(
+      fluidRow(column(12,
+                      sliderInput("obs",
+                                  "Number of Observations:",
+                                  min = 5,
+                                  max = 50000,
+                                  value = 20))),
+      fluidRow(column(12,
+                      radioButtons("dist", "Distribution type:",
+                                   c("Normal" = "Normal",
+                                     "Exponential" = "Exponential"))))),
+    # Show a plot of the generated distribution
+    
+    mainPanel(plotOutput("distPlot"))
+  )	 
+)
+)
